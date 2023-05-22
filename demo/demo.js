7b356cb9117c1701a04cb23f7c74bff16f68e43e
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2023-05-23 00:52:28 +0200

    Fix linting and prettier issues

--- demo/demo.js
+++ demo/demo.js
@@ -1,194 +1,4 @@
-"use strict";
-
-function main() {
-  let map = L.map('map', {
-    zoomControl: false
-  }).setView([0, 0], 2);
-
-  L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
-
-
-  let languageArray = [
-    L.langObject('de', 'Deutsch'),
-    L.langObject('en', 'English'),
-    L.langObject('eo', 'Esperanto'),
-    L.langObject('fi', 'Suomi'),
-    L.langObject('fr', 'Français'),
-    L.langObject('ko', '한국인'),
-    L.langObject('ru', 'Русский')
-  ];
-  let languageArrayWithIcons = [
-    L.langObject('de', 'Deutsch',   '../images/de.svg'),
-    L.langObject('en', 'English',   '../images/en.svg'),
-    L.langObject('eo', 'Esperanto', '../images/eo.svg'),
-    L.langObject('fi', 'Suomi',     '../images/fi.svg'),
-    L.langObject('fr', 'Français',  '../images/fr.svg'),
-    L.langObject('ko', '한국인',     '../images/ko.svg'),
-    L.langObject('ru', 'Русский',   '../images/ru.svg')
-  ];
-
-  // Add horizontal versions as button
-  L.languageSelector({
-    languages: languageArray,
-    callback: changeLanguage,
-    title: 'Language',
-    vertical: false,
-    position: 'topleft',
-    initialLanguage: 'en',
-    button: true
-  }).addTo(map);
-  L.languageSelector({
-    languages: languageArray,
-    callback: changeLanguage,
-    vertical: false,
-    position: 'topleft',
-    initialLanguage: 'en',
-    button: true
-  }).addTo(map);
-  L.languageSelector({
-    languages: languageArrayWithIcons,
-    callback: changeLanguage,
-    title: 'Language',
-    vertical: false,
-    position: 'topleft',
-    initialLanguage: 'en',
-    button: true
-  }).addTo(map);
-  L.languageSelector({
-    languages: languageArrayWithIcons,
-    callback: changeLanguage,
-    vertical: false,
-    position: 'topleft',
-    initialLanguage: 'en',
-    button: true
-  }).addTo(map);
-
-  // Add vertical versions as button 
-  L.languageSelector({
-    languages: languageArray,
-    callback: changeLanguage,
-    title: 'Language',
-    vertical: true,
-    position: 'topleft',
-    initialLanguage: 'en',
-    button: true
-  }).addTo(map);
-  L.languageSelector({
-    languages: languageArray,
-    callback: changeLanguage,
-    vertical: true,
-    position: 'topleft',
-    initialLanguage: 'en',
-    button: true
-  }).addTo(map);
-  L.languageSelector({
-    languages: languageArrayWithIcons,
-    callback: changeLanguage,
-    title: 'Language',
-    vertical: true,
-    position: 'topleft',
-    initialLanguage: 'en',
-    button: true
-  }).addTo(map);
-  L.languageSelector({
-    languages: languageArrayWithIcons,
-    callback: changeLanguage,
-    vertical: true,
-    position: 'topleft',
-    initialLanguage: 'en',
-    button: true
-  }).addTo(map);
-
-  // Add vertical versions without button
-  L.languageSelector({
-    languages: languageArray,
-    callback: changeLanguage,
-    vertical: true,
-    position: 'topright',
-    initialLanguage: 'en',
-    button: false
-  }).addTo(map);
-  L.languageSelector({
-    languages: languageArray,
-    callback: changeLanguage,
-    title: 'Language',
-    vertical: true,
-    position: 'topright',
-    initialLanguage: 'en',
-    button: false
-  }).addTo(map);
-  L.languageSelector({
-    languages: languageArrayWithIcons,
-    callback: changeLanguage,
-    vertical: true,
-    position: 'topright',
-    initialLanguage: 'en',
-    button: false
-  }).addTo(map);
-  L.languageSelector({
-    languages: languageArrayWithIcons,
-    callback: changeLanguage,
-    title: 'Language',
-    vertical: true,
-    position: 'topright',
-    initialLanguage: 'en',
-    button: false
-  }).addTo(map);
-
-  // Add horizontal versions without button
-  L.languageSelector({
-    languages: languageArray,
-    callback: changeLanguage,
-    vertical: false,
-    position: 'bottomleft',
-    initialLanguage: 'en',
-    button: false
-  }).addTo(map);
-  L.languageSelector({
-    languages: languageArray,
-    callback: changeLanguage,
-    title: 'Language',
-    vertical: false,
-    position: 'bottomleft',
-    initialLanguage: 'en',
-    button: false
-  }).addTo(map);
-  L.languageSelector({
-    languages: languageArrayWithIcons,
-    callback: changeLanguage,
-    vertical: false,
-    position: 'bottomleft',
-    initialLanguage: 'en',
-    button: false
-  }).addTo(map);
-  L.languageSelector({
-    languages: languageArrayWithIcons,
-    callback: changeLanguage,
-    title: 'Language',
-    vertical: false,
-    position: 'bottomleft',
-    initialLanguage: 'en',
-    button: false
-  }).addTo(map);
-
-}
-
-/**
- * Change the language
- * @param String id of the language
- */
-function changeLanguage(selectedLanguage) {
-  // Add or replace the language parameter of the URL
-  window.history.replaceState({}, '', updateURLParameter(window.location.href, 'lang', selectedLanguage));
-
-  // Set HTML lang attribut
-  document.body.parentElement.lang = selectedLanguage;
-
-  // Change the texts
-  document.getElementById('language').innerText = "Swiched to: '" + selectedLanguage + "'";
-}
+/* global L */
 
 /**
  * Add or replace a parameter (with value) in the given URL.
@@ -206,8 +16,8 @@ function updateURLParameter(url, param, paramVal) {
   let temp = "";
 
   if (additionalURL) {
-    let tmpAnchor = additionalURL.split("#");
-    let theParams = tmpAnchor[0];
+    const tmpAnchor = additionalURL.split("#");
+    const theParams = tmpAnchor[0];
     theAnchor = tmpAnchor[1];
     if (theAnchor) {
       additionalURL = theParams;
@@ -215,15 +25,15 @@ function updateURLParameter(url, param, paramVal) {
 
     tempArray = additionalURL.split("&");
 
-    for (let i = 0; i < tempArray.length; i++) {
-      if (tempArray[i].split('=')[0] != param) {
+    for (let i = 0; i < tempArray.length; i += 1) {
+      if (tempArray[i].split("=")[0] !== param) {
         newAdditionalURL += temp + tempArray[i];
         temp = "&";
       }
     }
   } else {
-    let tmpAnchor = baseURL.split("#");
-    let theParams = tmpAnchor[0];
+    const tmpAnchor = baseURL.split("#");
+    const theParams = tmpAnchor[0];
     theAnchor = tmpAnchor[1];
 
     if (theParams) {
@@ -232,12 +42,199 @@ function updateURLParameter(url, param, paramVal) {
   }
 
   if (theAnchor) {
-    paramVal += "#" + theAnchor;
+    paramVal += `#${theAnchor}`;
   }
 
-  let rows_txt = temp + "" + param + "=" + paramVal;
-  return baseURL + "?" + newAdditionalURL + rows_txt;
+  const rowsTxt = `${temp}${param}=${paramVal}`;
+  return `${baseURL}?${newAdditionalURL}${rowsTxt}`;
 }
 
+/**
+ * Change the language
+ * @param String id of the language
+ */
+function changeLanguage(selectedLanguage) {
+  // Add or replace the language parameter of the URL
+  window.history.replaceState({}, "", updateURLParameter(window.location.href, "lang", selectedLanguage));
+
+  // Set HTML lang attribut
+  document.body.parentElement.lang = selectedLanguage;
+
+  // Change the texts
+  document.getElementById("language").innerText = `Swiched to: '${selectedLanguage}'`;
+}
+
+function main() {
+  const map = L.map("map", {
+    zoomControl: false
+  }).setView([0, 0], 2);
+
+  L.tileLayer("https://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+    attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  const languageArray = [
+    L.langObject("de", "Deutsch"),
+    L.langObject("en", "English"),
+    L.langObject("eo", "Esperanto"),
+    L.langObject("fi", "Suomi"),
+    L.langObject("fr", "Français"),
+    L.langObject("ko", "한국인"),
+    L.langObject("ru", "Русский")
+  ];
+  const languageArrayWithIcons = [
+    L.langObject("de", "Deutsch", "../images/de.svg"),
+    L.langObject("en", "English", "../images/en.svg"),
+    L.langObject("eo", "Esperanto", "../images/eo.svg"),
+    L.langObject("fi", "Suomi", "../images/fi.svg"),
+    L.langObject("fr", "Français", "../images/fr.svg"),
+    L.langObject("ko", "한국인", "../images/ko.svg"),
+    L.langObject("ru", "Русский", "../images/ru.svg")
+  ];
+
+  // Add horizontal versions as button
+  L.languageSelector({
+    languages: languageArray,
+    callback: changeLanguage,
+    title: "Language",
+    vertical: false,
+    position: "topleft",
+    initialLanguage: "en",
+    button: true
+  }).addTo(map);
+  L.languageSelector({
+    languages: languageArray,
+    callback: changeLanguage,
+    vertical: false,
+    position: "topleft",
+    initialLanguage: "en",
+    button: true
+  }).addTo(map);
+  L.languageSelector({
+    languages: languageArrayWithIcons,
+    callback: changeLanguage,
+    title: "Language",
+    vertical: false,
+    position: "topleft",
+    initialLanguage: "en",
+    button: true
+  }).addTo(map);
+  L.languageSelector({
+    languages: languageArrayWithIcons,
+    callback: changeLanguage,
+    vertical: false,
+    position: "topleft",
+    initialLanguage: "en",
+    button: true
+  }).addTo(map);
+
+  // Add vertical versions as button
+  L.languageSelector({
+    languages: languageArray,
+    callback: changeLanguage,
+    title: "Language",
+    vertical: true,
+    position: "topleft",
+    initialLanguage: "en",
+    button: true
+  }).addTo(map);
+  L.languageSelector({
+    languages: languageArray,
+    callback: changeLanguage,
+    vertical: true,
+    position: "topleft",
+    initialLanguage: "en",
+    button: true
+  }).addTo(map);
+  L.languageSelector({
+    languages: languageArrayWithIcons,
+    callback: changeLanguage,
+    title: "Language",
+    vertical: true,
+    position: "topleft",
+    initialLanguage: "en",
+    button: true
+  }).addTo(map);
+  L.languageSelector({
+    languages: languageArrayWithIcons,
+    callback: changeLanguage,
+    vertical: true,
+    position: "topleft",
+    initialLanguage: "en",
+    button: true
+  }).addTo(map);
+
+  // Add vertical versions without button
+  L.languageSelector({
+    languages: languageArray,
+    callback: changeLanguage,
+    vertical: true,
+    position: "topright",
+    initialLanguage: "en",
+    button: false
+  }).addTo(map);
+  L.languageSelector({
+    languages: languageArray,
+    callback: changeLanguage,
+    title: "Language",
+    vertical: true,
+    position: "topright",
+    initialLanguage: "en",
+    button: false
+  }).addTo(map);
+  L.languageSelector({
+    languages: languageArrayWithIcons,
+    callback: changeLanguage,
+    vertical: true,
+    position: "topright",
+    initialLanguage: "en",
+    button: false
+  }).addTo(map);
+  L.languageSelector({
+    languages: languageArrayWithIcons,
+    callback: changeLanguage,
+    title: "Language",
+    vertical: true,
+    position: "topright",
+    initialLanguage: "en",
+    button: false
+  }).addTo(map);
+
+  // Add horizontal versions without button
+  L.languageSelector({
+    languages: languageArray,
+    callback: changeLanguage,
+    vertical: false,
+    position: "bottomleft",
+    initialLanguage: "en",
+    button: false
+  }).addTo(map);
+  L.languageSelector({
+    languages: languageArray,
+    callback: changeLanguage,
+    title: "Language",
+    vertical: false,
+    position: "bottomleft",
+    initialLanguage: "en",
+    button: false
+  }).addTo(map);
+  L.languageSelector({
+    languages: languageArrayWithIcons,
+    callback: changeLanguage,
+    vertical: false,
+    position: "bottomleft",
+    initialLanguage: "en",
+    button: false
+  }).addTo(map);
+  L.languageSelector({
+    languages: languageArrayWithIcons,
+    callback: changeLanguage,
+    title: "Language",
+    vertical: false,
+    position: "bottomleft",
+    initialLanguage: "en",
+    button: false
+  }).addTo(map);
+}
 
 main();
